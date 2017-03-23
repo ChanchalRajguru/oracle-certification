09aa08f9483a4a6c613e001300194f5fd064ae93
Mincong HUANG <mincong.h@gmail.com>
2017-03-23 22:31:19 +0100

    chapter-6: error handling

---
+++ ocajp/src/test/java/io/mincong/ocajp/chapter6/TryCatchFinallyTest.java
@@ -0,0 +1,33 @@
+package io.mincong.ocajp.chapter6;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * A statement is required to have a {@code catch} and / or {@code finally} clause. If it goes the
+ * {@code catch} route, it is allowed to have multiple {@code catch} clauses.
+ *
+ * @author Mincong Huang
+ */
+public class TryCatchFinallyTest {
+
+  @Test
+  public void testTryCatchCombination() {
+    try {
+      Integer.parseInt("A");
+    } catch (NumberFormatException e) {
+      assertEquals("For input string: \"A\"", e.getMessage());
+    }
+  }
+
+  @Test(expected = NumberFormatException.class)
+  public void testTryFinallyCombination() {
+    try {
+      Integer.parseInt("A");
+    } finally {
+      // do nothing
+    }
+  }
+
+}
