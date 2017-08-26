71614f9f906f1d65f3da634b47ad1c6c5c51483b
Mincong HUANG <mincong.h@gmail.com>
2017-08-26 15:58:22 +0200

    Chapter 5: Review notes II

--- ocpjp/src/test/java/io/mincong/ocpjp/regex/ReviewNoteTest.java
+++ ocpjp/src/test/java/io/mincong/ocpjp/regex/ReviewNoteTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.junit.Test;
@@ -345,7 +346,207 @@ public class ReviewNoteTest {
   }
 
   /* Search, parse, and build strings */
-  // TODO
+
+  /**
+   * Method {@link String#indexOf(int)} and its overloaded methods
+   * return the first matching position of a character or string,
+   * starting from the specified position of this string, or from its
+   * beginning.
+   */
+  @Test
+  public void indexOf() throws Exception {
+    assertThat("Hello world!".indexOf('o')).isEqualTo(4);
+    assertThat("Hello world!".indexOf('o', -1)).isEqualTo(4);
+    assertThat("Hello world!".indexOf('o', 4)).isEqualTo(4); // `fromIndex` is inclusive.
+    assertThat("Hello world!".indexOf('o', 5)).isEqualTo(7);
+    assertThat("Hello world!".indexOf('o', 100)).isEqualTo(-1); // Not found; no exception.
+
+    assertThat("Hello world!".indexOf("world")).isEqualTo(6);
+    assertThat("Hello world!".indexOf("world", -1)).isEqualTo(6);
+    assertThat("Hello world!".indexOf("world", 6)).isEqualTo(6);
+    assertThat("Hello world!".indexOf("world", 7)).isEqualTo(-1);
+  }
+
+  /**
+   * Method {@link String#lastIndexOf(int)} and its overloaded
+   * methods return the last <b>matching</b> position of a character
+   * in the entire string, or its subset (position 0 to the specified
+   * position).
+   * <p>
+   * Note that <tt>indexOf()</tt> and <tt>lastIndexOf()</tt> differ
+   * in the manner that they search a target string—<tt>indexOf()</tt>
+   * searches in increasing position numbers and
+   * <tt>lastIndexOf()</tt> searches backward. Due to this
+   * difference, <tt>indexOf('a', -100)</tt> will search the complete
+   * string, but <tt>lastIndexOf('a', -100)</tt> won't. In a similar
+   * manner, because <tt>lastIndexOf()</tt> searches backwards,
+   * <tt>lastIndexOf('a', 100)</tt> will search the string, but
+   * <tt>lastIndexOf('a', 0)</tt> or <tt>lastIndexOf('a', -100)</tt>
+   * won't.
+   * <p>
+   * Methods <tt>indexOf()</tt> and <tt>lastIndexOf()</tt> don't
+   * throw a compilation error or runtime exception if the search
+   * position is negative or greater than the length of the string.
+   * If no match is found, they return -1.
+   */
+  @Test
+  public void lastIndexOf() throws Exception {
+    assertThat("Hello world!".lastIndexOf('o')).isEqualTo(7);
+    assertThat("Hello world!".lastIndexOf('o', 7)).isEqualTo(7); // `fromIndex` is inclusive.
+    assertThat("Hello world!".lastIndexOf('o', 6)).isEqualTo(4);
+    assertThat("Hello world!".lastIndexOf('o', -100)).isEqualTo(-1); // Not found, no exception.
+
+    assertThat("Hello world!".lastIndexOf("world")).isEqualTo(6);
+    assertThat("Hello world!".lastIndexOf("world", 6)).isEqualTo(6);
+    assertThat("Hello world!".lastIndexOf("world", 5)).isEqualTo(-1);
+  }
+
+  /**
+   * Method {@link String#contains(CharSequence)} searches for an
+   * exact match in this string. Because <tt>contains()</tt> accepts
+   * a method parameter of interface {@link CharSequence}, you can
+   * pass to it both a {@link String} and a {@link StringBuilder}
+   * object.
+   */
+  @Test
+  public void contains() throws Exception {
+    assertThat("Hello world!".contains("Hello")).isTrue();
+    assertThat("Hello world!".contains(new StringBuilder("world"))).isTrue();
+  }
+
+  /**
+   * Method {@link String#subSequence(int, int)} and
+   * {@link String#substring(int)} accept <tt>int</tt> parameters and
+   * return a substring of the target string.
+   * <p>
+   * Method <tt>substring()</tt> defines overloaded versions, which
+   * accept one or two <tt>int</tt> method parameters and return a
+   * substring of the target string.
+   * <p>
+   * The name of methods <tt>subSequence()</tt>, <tt>substring()</tt>
+   * can be used to determine their return type.
+   * <ul>
+   * <li><tt>subSequence()</tt> returns {@link CharSequence}
+   * <li><tt>substring()</tt> returns {@link String}
+   * </ul>
+   */
+  @Test
+  public void subSequenceAndSubstring() throws Exception {
+    String str = "周六";
+    StringBuilder sb = new StringBuilder("周日");
+
+    // String#subSequence(int, int)
+    assertThat(str.subSequence(0, 1)).isEqualTo("周");
+    assertThat(sb.subSequence(0, 1)).isEqualTo("周");
+
+    // String#substring(int, int)
+    assertThat(str.substring(0, 1)).isEqualTo("周");
+    assertThat(sb.substring(0, 1)).isEqualTo("周");
+
+    // String#substring(int)
+    assertThat(str.substring(1)).isEqualTo("六");
+    assertThat(sb.substring(1)).isEqualTo("日");
+  }
+
+  /**
+   * Method <tt>split()</tt> searches for a matching regex pattern
+   * and split a {@link String} into an array of string values.
+   */
+  @Test
+  public void split() throws Exception {
+    String[] arr = "Regex is cool".split(" ");
+    assertThat(arr).containsExactly("Regex", "is", "cool");
+
+    arr = "1,2.3!4'5".split("[,.!']");
+    assertThat(arr).containsExactly("1", "2", "3", "4", "5");
+
+    arr = "1,2.3!4'5".split("[,.!']", 3); // limit = 3
+    assertThat(arr).containsExactly("1", "2", "3!4'5");
+  }
+
+  /**
+   * {@link String#replace(CharSequence, CharSequence)} returns a new
+   * {@link String} resulting from finding and replacing each
+   * substring of the string that matches the old target sequence
+   * with the specified new replacement sequence.
+   */
+  @Test
+  public void replace() throws Exception {
+    assertThat("星期六".replace("星期", "周")).isEqualTo("周六");
+    assertThat("星期六".replace("星期", new StringBuffer("周"))).isEqualTo("周六");
+    assertThat("星期六".replace("星期", new StringBuilder("周"))).isEqualTo("周六");
+  }
+
+  /**
+   * {@link String#replaceAll(String, String)} replaces each
+   * substring of the string that matches the given regular
+   * expression with the given replacement.
+   */
+  @Test
+  public void replaceAll() throws Exception {
+    String oldStr = "{\"keyA\": 10, \"keyB\": 20}";
+    String newStr = oldStr.replaceAll("\\d+", "?");
+    assertThat(newStr).isEqualTo("{\"keyA\": ?, \"keyB\": ?}");
+  }
+
+  /**
+   * {@link String#replaceFirst(String, String)} replaces the first
+   * substring of the string that matches the given regular
+   * expression with the given replacement.
+   */
+  @Test
+  public void replaceFirst() throws Exception {
+    String oldStr = "{\"keyA\": 10, \"keyB\": 20}";
+    String newStr = oldStr.replaceFirst("\\d+", "?");
+    assertThat(newStr).isEqualTo("{\"keyA\": ?, \"keyB\": 20}");
+  }
+
+  /**
+   * {@link Scanner} can be used to parse and tokenize strings.
+   * <ul>
+   * <li>If no delimiter is specified, a pattern that matches
+   * whitespace is used by default for a {@link Scanner} object.
+   * <li>You can specify a custom delimiter by calling its method
+   * {@link Scanner#useDelimiter(String)} with a regex.
+   * </ul>
+   */
+  @Test
+  public void scanner() throws Exception {
+    Scanner scanner;
+
+    // use delimiter
+    scanner = new Scanner("/usr:/bin:/home:/var");
+    scanner.useDelimiter(":");
+    List<String> paths = new ArrayList<>();
+    while (scanner.hasNext()) {
+      paths.add(scanner.next());
+    }
+    assertThat(paths).containsExactly("/usr", "/bin", "/home", "/var");
+
+    // nextXxx: String
+    scanner = new Scanner("Hello world");
+    List<String> stringList = new ArrayList<>();
+    while (scanner.hasNext()) {
+      stringList.add(scanner.next());
+    }
+    assertThat(stringList).containsExactly("Hello", "world");
+
+    // nextXxx: Integer
+    scanner = new Scanner("1 2 3");
+    List<Integer> integers = new ArrayList<>();
+    while (scanner.hasNextInt()) {
+      integers.add(scanner.nextInt());
+    }
+    assertThat(integers).containsExactly(1, 2, 3);
+
+    // nextXxx: Double
+    scanner = new Scanner("1.0 2.0 3.0");
+    List<Double> doubles = new ArrayList<>();
+    while (scanner.hasNextDouble()) {
+      doubles.add(scanner.nextDouble());
+    }
+    assertThat(doubles).containsExactly(1.0, 2.0, 3.0);
+  }
 
   /* Formatting strings */
   // TODO
