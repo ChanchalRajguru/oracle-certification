559c9a5ad22d8daf967d9c5bf9dd0fcd8d1a67da
Mincong HUANG <mincong.h@gmail.com>
2017-12-15 10:34:41 +0100

    Chapter-9: Test exceptions in DriverManager

--- ocpjp/src/test/java/io/mincong/ocpjp/jdbc/DriverManagerTest.java
+++ ocpjp/src/test/java/io/mincong/ocpjp/jdbc/DriverManagerTest.java
@@ -1,8 +1,14 @@
 package io.mincong.ocpjp.jdbc;
 
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -37,19 +43,46 @@ public class DriverManagerTest {
     connection.close();
   }
 
+  @Test(expected = SQLException.class)
+  public void noSuitableDriver() throws Exception {
+    DriverManager.getConnection("jdbc:mysql://localhost");
+  }
+
+  @Test(expected = SQLException.class)
+  public void accessDenied() throws Exception {
+    DriverManager.getConnection(URL, "foo", "bar");
+  }
+
   @Test
   public void createTable() throws Exception {
-    Statement statement = connection.createStatement();
-    statement.executeUpdate(
-        "CREATE TABLE book ("
-            + "  id INT PRIMARY KEY,"
-            + "  title VARCHAR(1000),"
-            + "  author CHAR(255),"
-            + "  publication_year INT,"
-            + "  unit_price REAL"
-            + ")"
-    );
-    statement.close();
+    StringBuilder sb = new StringBuilder();
+    try (Statement s = connection.createStatement()) {
+      s.executeUpdate(
+          "CREATE TABLE book ("
+              + "  id INT PRIMARY KEY,"
+              + "  title VARCHAR(1000),"
+              + "  author CHAR(255),"
+              + "  publication_year INT,"
+              + "  unit_price REAL"
+              + ")"
+      );
+      try (ResultSet rs = s.executeQuery("SHOW TABLES")) {
+        while (rs.next()) {
+          sb.append(getRow(rs));
+        }
+      }
+    }
+    assertThat(sb.toString()).contains("BOOK");
+  }
+
+  private String getRow(ResultSet rs) throws SQLException {
+    int max = rs.getMetaData().getColumnCount();
+    List<String> row = new ArrayList<>(max);
+    // Database index starts at 1.
+    for (int i = 1; i <= max; i++) {
+      row.add(rs.getString(i));
+    }
+    return String.join(", ", row);
   }
 
 }
