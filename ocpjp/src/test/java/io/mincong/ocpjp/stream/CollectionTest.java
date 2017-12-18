3190675f9ccf7f9d084555dff9df5af5dd7a89c7
Mincong HUANG <mincong.h@gmail.com>
2017-12-18 22:45:23 +0100

    Stream API

--- ocpjp/src/test/java/io/mincong/ocpjp/stream/CollectionTest.java
+++ ocpjp/src/test/java/io/mincong/ocpjp/stream/CollectionTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.junit.Before;
@@ -22,6 +23,8 @@ public class CollectionTest {
     books.add(new Book("A"));
     books.add(new Book("B"));
   }
+
+  /* Collection-related tests */
 
   @Test
   public void forEach_list() throws Exception {
@@ -56,6 +59,73 @@ public class CollectionTest {
         .filter(b -> b.getName().startsWith("A"))
         .collect(Collectors.toList());
     assertThat(filtered).containsExactly(new Book("A"));
+  }
+
+  /* Stream API related tests */
+
+  @Test
+  public void peek() throws Exception {
+    List<Integer> positives = new ArrayList<>();
+    int sum = Stream.of(-1, 0, 1, 2, 3)
+        .filter(i -> i > 0)
+        .peek(positives::add)
+        .mapToInt(Integer::intValue)
+        .sum();
+
+    assertThat(positives).containsExactly(1, 2, 3);
+    assertThat(sum).isEqualTo(6);
+  }
+
+  @Test
+  public void map() throws Exception {
+    String s = Stream.of(-1, 0, 1, 2, 3)
+        .filter(i -> i > 0)
+        .map(String::valueOf)
+        .collect(Collectors.joining(", "));
+    assertThat(s).isEqualTo("1, 2, 3");
+  }
+
+  @Test
+  public void findFirst_normalStream() throws Exception {
+    Optional<String> first = Stream.of("A", "B").findFirst();
+    assertThat(first.isPresent()).isTrue();
+    assertThat(first.orElse("C")).isEqualTo("A");
+  }
+
+  @Test
+  public void findFirst_emptyStream() throws Exception {
+    Optional<String> first = new ArrayList<String>().stream().findFirst();
+    assertThat(first.isPresent()).isFalse();
+  }
+
+  @Test
+  public void findAny_normalStream() throws Exception {
+    Optional<String> any = Stream.of("A", "B").findAny();
+    assertThat(any.isPresent()).isTrue();
+  }
+
+  @Test
+  public void findAny_emptyStream() throws Exception {
+    Optional<String> any = new ArrayList<String>().stream().findAny();
+    assertThat(any.isPresent()).isFalse();
+  }
+
+  @Test
+  public void anyMatch() throws Exception {
+    boolean hasMatched = Stream.of(-1, -2, 3).anyMatch(i -> i > 0);
+    assertThat(hasMatched).isTrue();
+  }
+
+  @Test
+  public void allMatch() throws Exception {
+    assertThat(Stream.of(-1, -2, 3).allMatch(i -> i > 0)).isFalse();
+    assertThat(Stream.of(-1, -2, 3).allMatch(i -> i < 4)).isTrue();
+  }
+
+  @Test
+  public void noneMatch() throws Exception {
+    assertThat(Stream.of(-1, -2, 3).noneMatch(i -> i == 0)).isTrue();
+    assertThat(Stream.of(-1, -2, 3).noneMatch(i -> i < 10)).isFalse();
   }
 
   private static class Book {
